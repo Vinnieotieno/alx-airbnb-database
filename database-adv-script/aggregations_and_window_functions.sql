0f8dbe458e3551c8b4a4a5cabcf86d7cf3413bc3
Vinnieotieno <121629812+Vinnieotieno@users.noreply.github.com>
2025-05-14 15:57:32 +0300

    Create aggregations_and_window_functions.sql

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,16 @@
+Write a query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.
+
+SELECT user_id, COUNT(*) AS total_bookings
+FROM bookings
+GROUP BY user_id
+ORDER BY total_bookings DESC;
+
+
+Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
+
+SELECT property_id, COUNT(*) AS booking_count,
+       RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM bookings
+GROUP BY property_id
+ORDER BY rank;
+
