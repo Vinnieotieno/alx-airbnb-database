1a88336710afe92e3d621222e09a85c77e88e235
Vinnieotieno <121629812+Vinnieotieno@users.noreply.github.com>
2025-05-14 16:12:47 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,1 +1,19 @@
 
+EXPLAIN ANALYZE
+SELECT * FROM bookings
+WHERE user_id = 123;
+
+-- Example: After Index
+EXPLAIN ANALYZE
+SELECT * FROM bookings
+WHERE user_id = 123;
+
+-- Example: Without Index
+EXPLAIN ANALYZE
+SELECT * FROM properties
+WHERE location = 'New York';
+
+-- Example: After Index
+EXPLAIN ANALYZE
+SELECT * FROM properties
+WHERE location = 'Nakuru';
